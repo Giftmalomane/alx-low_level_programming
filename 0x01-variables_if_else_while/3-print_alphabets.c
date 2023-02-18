2512aa63feca2e1c5dc65fa81bdb61bc45f2f3da
Gift <malomanegift@gmail.com>
2023-02-17 16:00:55 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,14 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+	
 		putchar(ch);
-	}
+
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
+	
 		putchar(ch);
-	}
+	
 	putchar(\n);
 
 	return (0);
