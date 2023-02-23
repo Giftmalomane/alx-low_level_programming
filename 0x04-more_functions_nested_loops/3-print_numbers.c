374fae52db05ab9e5aa66b612a2e0bfbad69c117
Gift <malomanegift@gmail.com>
2023-02-23 06:54:02 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
- * return: numbers from 0 to 9
+ * print_numbers - prints numbers from 0 up to 9
+ * return: numbers from 0 up to 9
  */
 void print_numbers(void)
 {
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar("\n");
 }
