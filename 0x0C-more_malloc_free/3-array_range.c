211d2e4fdbabcad219d2140e41121e6ea6596854
Gift <malomanegift@gmail.com>
2023-03-16 17:22:37 -0700

    array of intergers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,22 +11,25 @@
 
 int *array_range(int min, int max)
 {
-	int *p;
+	int *ptr;
 
 	int i, size;
 
 	if (min > max)
+
 		return (NULL);
 
 	size = max - min + 1;
 
-	p = malloc(sizeof(int) * size);
+	ptr = malloc(sizeof(int) * size);
 
-	if (p == NULL)
+	if (ptr == NULL)
+
 		return (NULL);
 
 	for (i = 0; min <= max; i++)
-		p[i] = min++;
-		return (p);
 
+		ptr[i] = min++;
+
+	return (ptr);
 }
